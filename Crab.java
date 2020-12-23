309fbc4e1258c1e63e427a2752b5f8bef401e3a6
Bruce Gustin <bgustin@srcs.org>
2020-12-23 10:13:22 -0800

    Update on Activity 2

--- Crab.java
+++ Crab.java
@@ -28,12 +28,26 @@ public class Crab extends Actor
     // Checks for user key presses so user can turn the Crab
     private void checkKeyPress()
     {
+        if(Greenfoot.isKeyDown("right"))
+        {
+            turn(4);
+        }
+        
+        if(Greenfoot.isKeyDown("left"))
+        {
+            turn(-4);
+        }
     
     }
     
     // Check for collisions with other objects
     private void onCollision()
     {
+        if(isTouching(Worm.class))
+        {
+            removeTouching(Worm.class);
+            Greenfoot.playSound("slurp.wav");
+        }
     
     }
 }
--- CrabWorld.java
+++ CrabWorld.java
@@ -20,5 +20,21 @@ public class CrabWorld extends World
     {
         Crab crab = new Crab();
         addObject(crab,198,211);
+        Worm worm = new Worm();
+        addObject(worm,49,61);
+        Worm worm2 = new Worm();
+        addObject(worm2,369,106);
+        Worm worm3 = new Worm();
+        addObject(worm3,197,297);
+        Worm worm4 = new Worm();
+        addObject(worm4,422,385);
+        Worm worm5 = new Worm();
+        addObject(worm5,73,396);
+        Worm worm6 = new Worm();
+        addObject(worm6,192,382);
+        Worm worm7 = new Worm();
+        addObject(worm7,232,498);
+        Worm worm8 = new Worm();
+        addObject(worm8,124,492);
     }
 }
